8fa9436ad2a0a81985e842ce95d863959142c5f1
Dmitrii Pisarenko <dp@dpisarenko.com>
2021-11-13 23:08:04 +0100

    Migrating to our own namespace.

--- project.clj
+++ project.clj
@@ -40,7 +40,7 @@
                                    ;; started and complied your application.
                                    ;; Comment this out once it no longer serves you.
                                    :open-urls ["http://localhost:3449/cards.html"]}
-                        :compiler { :main       "hello-devcards.core"
+                        :compiler { :main       "dpisarenko.ui.core"
                                     :asset-path "js/compiled/devcards_out"
                                     :output-to  "resources/public/js/compiled/hello_devcards_devcards.js"
                                     :output-dir "resources/public/js/compiled/devcards_out"
